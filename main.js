3ed6c79f51bc51e601e6acd28cf3cbc475835f3a
samboxft <30531932+samboxft@users.noreply.github.com>
2020-09-28 18:42:43 +0300

    Add files via upload
    RocketSHip start

---
+++ main.js
@@ -0,0 +1,25 @@
+let car = {
+    make: 'VW',
+    type: 'polo',
+    color: 'blue',
+    isTurnedOn: false,
+    numberOfWheels: 4,
+    seats: ['seat1', 'seat2', 'seat3', 'seat4'],
+    turnOn: function () {
+        this.isTurnedOn = true;
+    },
+    fly: function () {
+        alert('fly');
+    },
+    switchCar: function (isOn) {
+        console.log('turn car' + isOn)
+        if (isOn == true) {
+            this.isTurnedOn = true;
+        } else {
+            this.isTurnedOn = false;
+        }
+    }
+};
+
+
+
